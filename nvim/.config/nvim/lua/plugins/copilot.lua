a59a11c07c64a4ec8ee0e35df1bf53cdaadd93cf
Khalid Alansary <khaled.ansary2003@outlook.com>
2025-03-14 13:41:24 +0200

    keep all copilot related config in copilot.lua

--- nvim/.config/nvim/lua/plugins/copilot.lua
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -1,3 +1,8 @@
 return {
 	"github/copilot.vim",
+	event = { "BufReadPre", "BufNewFile" },
+	keys = {
+		{ "<leader>cpe", ":Copilot enable<CR>", desc = "Enable Copilot" },
+		{ "<leader>cpd", ":Copilot disable<CR>", desc = "Disable Copilot" },
+	},
 }
