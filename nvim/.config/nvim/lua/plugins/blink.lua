cf54ce5247c8c5825e3c5ad8e1cca85e54ce6ae3
Khalid Alansary <khaled.ansary2003@outlook.com>
2025-05-19 08:23:32 +0300

    chore(nvim): remove wezterm-types from dependencies

--- nvim/.config/nvim/lua/plugins/blink.lua
+++ nvim/.config/nvim/lua/plugins/blink.lua
@@ -4,16 +4,9 @@ return {
 		"rafamadriz/friendly-snippets",
 		{
 			"folke/lazydev.nvim",
-			dependencies = {
-				"justinsgithub/wezterm-types",
-			},
 			ft = "lua",
 			opts = {
-				library = {
-					"lazy.nvim",
-					{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
-					{ path = "wezterm-types", mods = { "wezterm" } },
-				},
+				library = { "lazy.nvim", { path = "${3rd}/luv/library", words = { "vim%.uv" } } },
 			},
 		},
 	},
