337862909fab4538304611a64548b37f106cd5fa
Khalid Alansary <khaled.ansary2003@outlook.com>
2025-07-06 14:14:06 +0300

    feat(nvim): disable copilot on startup

--- dot_config/nvim/lua/plugins/copilot.lua
+++ dot_config/nvim/lua/plugins/copilot.lua
@@ -1,8 +1,12 @@
 return {
 	"github/copilot.vim",
-	event = { "BufReadPre", "BufNewFile" },
+	event = "VeryLazy",
 	keys = {
 		{ "<leader>cpe", ":Copilot enable<CR>", desc = "Enable Copilot" },
 		{ "<leader>cpd", ":Copilot disable<CR>", desc = "Disable Copilot" },
 	},
+	config = function()
+		-- Start disabled by default
+		vim.cmd.Copilot("disable")
+	end,
 }
