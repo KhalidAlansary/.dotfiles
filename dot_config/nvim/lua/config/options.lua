6e1054a4341191238c5c000d117afeb43fcfabb4
Khalid Alansary <khaled.ansary2003@outlook.com>
2025-07-20 12:32:48 +0300

    feat(nvim): i'm a tab guy now

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -3,9 +3,6 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 
 vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.expandtab = true
-vim.opt.autoindent = true
 vim.opt.wrap = false
 
 vim.opt.ignorecase = true
