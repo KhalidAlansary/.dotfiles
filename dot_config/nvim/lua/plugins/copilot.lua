9db3baf03aebdc3ced51bcea093b0c1e4d6af18e
Khalid Alansary <khaled.ansary2003@outlook.com>
2025-07-26 07:09:09 +0300

    Revert "feat(nvim): disable copilot on startup"
    This reverts commit 337862909fab4538304611a64548b37f106cd5fa.

--- dot_config/nvim/lua/plugins/copilot.lua
+++ dot_config/nvim/lua/plugins/copilot.lua
@@ -1,12 +1,8 @@
 return {
 	"github/copilot.vim",
-	event = "VeryLazy",
+	event = { "BufReadPre", "BufNewFile" },
 	keys = {
 		{ "<leader>cpe", ":Copilot enable<CR>", desc = "Enable Copilot" },
 		{ "<leader>cpd", ":Copilot disable<CR>", desc = "Disable Copilot" },
 	},
-	config = function()
-		-- Start disabled by default
-		vim.cmd.Copilot("disable")
-	end,
 }
