3bdc074e34784c8c3c141fc3f0b4cb240b87d754
Khalid Alansary <khaled.ansary2003@outlook.com>
2025-08-12 18:02:39 +0300

    feat(hypr): add keyboard layout scripts for games

---
+++ dot_config/hypr/scripts/executable_dvp.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+hyprctl --batch "keyword input:kb_variant dvp,; keyword input:kb_layout us,eg; keyword input:resolve_binds_by_sym 0"
---
+++ dot_config/hypr/scripts/executable_qwerty.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+hyprctl --batch "keyword input:kb_layout us,us,eg; keyword input:kb_variant ,dvp; keyword input:resolve_binds_by_sym 1"
