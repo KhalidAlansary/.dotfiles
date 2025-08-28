de77f0748f6546349e8be577d9fe2686a997e8ec
Khalid Alansary <khaled.ansary2003@outlook.com>
2025-08-28 16:18:21 +0300

    chore(nvim): clean unused configurations in CopilotChat

--- dot_config/nvim/lua/plugins/CopilotChat.lua
+++ dot_config/nvim/lua/plugins/CopilotChat.lua
@@ -24,25 +24,6 @@ return {
 	},
 	keys = {
 		{
-			"<leader>ccq",
-			function()
-				local input = vim.fn.input("Quick Chat: ")
-				if input ~= "" then
-					require("CopilotChat").ask(input, { selection = require("CopilotChat.select").buffer })
-				end
-			end,
-			desc = "CopilotChat - Quick chat",
-		},
-		-- Show prompts actions with telescope
-		{
-			"<leader>ccp",
-			function()
-				local actions = require("CopilotChat.actions")
-				require("CopilotChat.integrations.telescope").pick(actions.prompt_actions())
-			end,
-			desc = "CopilotChat - Prompt actions",
-		},
-		{
 			"<leader>cco",
 			vim.cmd.CopilotChatOpen,
 			desc = "Open chat window",
@@ -53,7 +34,4 @@ return {
 			desc = "Toggle chat window",
 		},
 	},
-	config = function(_, opts)
-		require("CopilotChat").setup(opts)
-	end,
 }
