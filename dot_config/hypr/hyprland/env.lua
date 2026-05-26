9d70f1d7bb1127847f13fa970951b60c1f9a02ea
Khalid Alansary <khaled.ansary2003@outlook.com>
2026-05-26 19:35:31 +0300

    chore(hypr): port some configs to lua

--- dot_config/hypr/hyprland/env.lua
+++ dot_config/hypr/hyprland/env.lua
@@ -1,2 +1,4 @@
 hl.env("XCURSOR_SIZE", "24")
 hl.env("HYPRCURSOR_SIZE", "24")
+hl.env("AQ_DRM_DEVICES", "/dev/dri/card0:/dev/dri/card1")
+hl.env("SSH_AUTH_SOCK", os.getenv("XDG_RUNTIME_DIR") .. "/ssh-agent.socket")
